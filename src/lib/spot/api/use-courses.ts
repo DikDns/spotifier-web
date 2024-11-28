9e7fa7e36879cf7e614b7efd3a9a7d1dc5df6fd7
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-11-28 19:56:38 +0700

    feat: add local storage in use courses

--- src/lib/spot/api/use-courses.ts
+++ src/lib/spot/api/use-courses.ts
@@ -1,11 +1,26 @@
 "use client";
 
-import { getCourses } from "@/lib/spot/courses";
+import { useEffect } from "react";
+
+import { useLocalStorage } from "@/lib/hooks/use-local-storage";
+import { type Course, getCourses } from "@/lib/spot/courses";
 import { useQuery } from "@tanstack/react-query";
 
 export const useCourses = () => {
-  return useQuery({
+  const [courses, setCourses] = useLocalStorage<Course[]>("courses", []);
+
+  const query = useQuery({
     queryKey: ["courses"],
     queryFn: getCourses,
+    initialData: courses,
   });
+
+  useEffect(() => {
+    if (query.data) {
+      setCourses(query.data);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query.data]);
+
+  return query;
 };
