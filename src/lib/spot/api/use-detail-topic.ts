f688f181a0306e7bab107982a7f1017c5720f5a1
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-11-28 19:57:50 +0700

    feat: add local storage in use detail topic

--- src/lib/spot/api/use-detail-topic.ts
+++ src/lib/spot/api/use-detail-topic.ts
@@ -1,11 +1,29 @@
 "use client";
 
-import { getDetailTopic } from "@/lib/spot/detail-topic";
+import { useEffect } from "react";
+
+import { useLocalStorage } from "@/lib/hooks/use-local-storage";
+import { type DetailTopic, getDetailTopic } from "@/lib/spot/detail-topic";
 import { useQuery } from "@tanstack/react-query";
 
 export const useDetailTopic = (courseId: string, topicId: string) => {
-  return useQuery({
+  const [detailTopic, setDetailTopic] = useLocalStorage<DetailTopic>(
+    `detail-topic-${courseId}-${topicId}`,
+    undefined,
+  );
+  const query = useQuery({
     queryKey: ["detail-topic", courseId, topicId],
     queryFn: () => getDetailTopic(courseId, topicId),
+    initialData: detailTopic,
   });
+
+  useEffect(() => {
+    if (query.data) {
+      setDetailTopic(query.data);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query.data]);
+
+  return query;
 };
