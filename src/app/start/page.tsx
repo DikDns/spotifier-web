c50830bcf30850e367417b4fbc714aef95764f5c
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2024-12-01 19:56:18 +0700

    fix: useSearchParams() should be wrapped in a suspense boundary

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import { Suspense } from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
-export default function StartPage() {
+function CallbackUrlButton() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl");
 
@@ -15,11 +16,19 @@ export default function StartPage() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
-      <h1 className="text-2xl font-bold">Start</h1>
-      <Link href={spotLoginUrl.toString()}>
-        <Button>Login with SPOT</Button>
-      </Link>
-    </main>
+    <Link href={spotLoginUrl.toString()}>
+      <Button>Login with SPOT</Button>
+    </Link>
+  );
+}
+
+export default function StartPage() {
+  return (
+    <Suspense>
+      <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">Start</h1>
+        <CallbackUrlButton />
+      </main>
+    </Suspense>
   );
 }
